c7f2a878dadea148969f80df287bb58cd8a2effa
Ezra Audivano Dirfa <ezra@skyshi.com>
2023-08-27 23:46:09 +0700

    style: add border bottom on active menu navbar

--- src/components/Navbar/index.tsx
+++ src/components/Navbar/index.tsx
@@ -8,12 +8,17 @@ import { RxHamburgerMenu } from "react-icons/rx";
 import useIndex from "./index.hook";
 import MobileMenu from "./components/MobileMenu";
 import Tooltip from "../Tooltip";
+import { usePathname, useRouter } from "next/navigation";
+import Container from "../Container";
+import clsx from "clsx";
 
 const Navbar = () => {
   const { isMenuOpen, setIsMenuOpen } = useIndex();
+  const router = useRouter();
+  const pathname = usePathname();
 
   return (
-    <div className="py-3 md:py-6 flex justify-between items-center">
+    <div className="sticky top-0 z-30 py-3 bg-white md:py-6 flex justify-between items-center">
       <div className="block md:hidden">
         <div
           className="cursor-pointer "
@@ -21,7 +26,6 @@ const Navbar = () => {
         >
           <RxHamburgerMenu />
         </div>
-
         <MobileMenu
           visible={isMenuOpen}
           onCloseMenu={() => setIsMenuOpen(false)}
@@ -38,7 +42,21 @@ const Navbar = () => {
           {MENUS.map((menu, key) => (
             <div key={key}>
               <Tooltip content={menu.description}>
-                <p className="cursor-pointer hover:font-medium">{menu.label}</p>
+                <div className="flex items-center flex-col">
+                  <p
+                    className={clsx(
+                      "cursor-pointer hover:font-semibold w-20 text-center"
+                    )}
+                    onClick={() => router.push(menu.route)}
+                  >
+                    {menu.label}
+                  </p>
+                  <div
+                    className={clsx("w-1/3 h-[2px] transition-all mt-1", {
+                      "bg-primary translate-x-0": pathname === menu.route,
+                    })}
+                  ></div>
+                </div>
               </Tooltip>
             </div>
           ))}
