3a2218c663ceeb3e6904fc4dab71847600c17420
Ezra Audivano Dirfa <ezra@skyshi.com>
2023-08-28 19:11:01 +0700

    fix: add rederiction path on coming soon component

--- src/components/ComingSoon/index.tsx
+++ src/components/ComingSoon/index.tsx
@@ -1,7 +1,11 @@
+"use client";
 import React from "react";
 import Button from "../Button";
+import { useRouter } from "next/navigation";
+import { ROUTES } from "@/constants/routes";
 
 const ComingSoon = () => {
+  const router = useRouter();
   return (
     <div className="w-full flex-col h-[60vh] flex items-center justify-center gap-6">
       <p className="uppercase font-bold text-7xl tracking-widest text-center">
@@ -10,7 +14,7 @@ const ComingSoon = () => {
       <p className=" text-gray-400">
         Exciting things are on the way! Stay tuned for updates.
       </p>
-      <Button>Back to Blog</Button>
+      <Button onClick={() => router.push(ROUTES.BLOG)}>Back to Blog</Button>
     </div>
   );
 };
