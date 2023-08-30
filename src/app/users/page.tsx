ea037225eeb3dcd5c313b29bb755d38095014a1c
Ezra Audivano Dirfa <ezra@skyshi.com>
2023-08-30 18:57:37 +0700

    fix: document title on user page

--- src/app/users/page.tsx
+++ src/app/users/page.tsx
@@ -4,13 +4,12 @@ import UsersComponent from "@/routes/Users";
 import Head from "next/head";
 
 const Users = () => {
-  document.title = "LuminaLife Blog | Users";
-  
   return (
     <>
       <Head>
         <title>LuminaLife Blog | Users</title>
       </Head>
+      
       <PageLayout isSSR={false}>
         <UsersComponent />
       </PageLayout>
