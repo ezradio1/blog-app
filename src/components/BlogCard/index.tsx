3aa7a343c36231509ea03bd1c6fa586a362f3d9d
Ezra Audivano Dirfa <ezra@skyshi.com>
2023-08-30 18:10:53 +0700

    refactor: change navigation on blog card and headline using Link

--- src/components/BlogCard/index.tsx
+++ src/components/BlogCard/index.tsx
@@ -1,44 +1,38 @@
-"use client"
 import Image from "next/image";
 import Link from "next/link";
 import type { BlogCardProps } from "./index.types";
-import { useRouter } from "next/navigation";
 import { ROUTES } from "@/constants/routes";
 
 const BlogCard = (props: BlogCardProps) => {
   const { id, title, body, image } = props;
-  const router = useRouter()
 
   return (
-    <div
-      className="group h-96 relative cursor-pointer transition-all duration-500 ease-out"
-      onClick={() => router.push(`${ROUTES.BLOG_DETAIL}/${id}`)}
-    >
-      <div className="h-[65%] relative rounded">
-        <Image
-          src={image}
-          alt="blog-image"
-          fill
-          objectFit="cover"
-          className="rounded"
-        />
-      </div>
+    <Link href={`${ROUTES.BLOG_DETAIL}/${id}`}>
+      <div className="group h-96 relative cursor-pointer transition-all duration-500 ease-out">
+        <div className="h-[65%] relative rounded">
+          <Image
+            src={image}
+            alt="blog-image"
+            fill
+            objectFit="cover"
+            className="rounded"
+          />
+        </div>
 
-      <div className="p-2 mt-2 flex flex-col gap-2">
-        <p className="leading-tight font-medium line-clamp-2 text-primary">
-          {title}
-        </p>
-        <p className="leading-tight text-[10px] text-gray-400 line-clamp-4">
-          {body}
-        </p>
-      </div>
+        <div className="p-2 mt-2 flex flex-col gap-2">
+          <p className="leading-tight font-medium line-clamp-2 text-primary">
+            {title}
+          </p>
+          <p className="leading-tight text-[10px] text-gray-400 line-clamp-4">
+            {body}
+          </p>
+        </div>
 
-      <Link href="/blog/:id">
         <div className="h-96 absolute transition-all flex justify-center items-center bg-black bg-opacity-10 w-full top-0 group-hover:translate-y-0 group-hover:opacity-100 opacity-0 -translate-y-4">
           <p className="text-white text-xs">Read Article</p>
         </div>
-      </Link>
-    </div>
+      </div>
+    </Link>
   );
 };
 
--- src/routes/Dashboard/components/Headline/index.tsx
+++ src/routes/Dashboard/components/Headline/index.tsx
@@ -1,31 +1,28 @@
-"use client"
 import { ROUTES } from "@/constants/routes";
 import Image from "next/image";
-import { useRouter } from "next/navigation";
 import type { HeadlineProps } from "./index.types";
+import Link from "next/link";
 
 const Headline = (props: HeadlineProps) => {
   const { id, image, title, body } = props;
-  const router = useRouter();
 
   return (
-    <div
-      className="w-full cursor-pointer"
-      onClick={() => router.push(`${ROUTES.BLOG_DETAIL}/${id}`)}
-    >
-      <div className="group border relative w-full h-96">
-        <Image src={image} alt="blog-image" fill objectFit="cover" />
-        <div className="h-full z-0 w-full absolute bg-gradient-to-b from-transparent to-black opacity-60"></div>
-        <div className="absolute bottom-1 px-4 py-3 transition-all duration-500 ease-out group-hover:translate-y-1 translate-y-14">
-          <p className="text-white font-semibold text-3xl z-10 line-clamp-2">
-            {title}
-          </p>
-          <p className="text-gray-200 text-justify mt-2 text-xs group-hover:opacity-100 opacity-0 transition-all z-10 line-clamp-3">
-            {body}
-          </p>
+    <Link href={`${ROUTES.BLOG_DETAIL}/${id}`}>
+      <div className="w-full cursor-pointer">
+        <div className="group border relative w-full h-96">
+          <Image src={image} alt="blog-image" fill objectFit="cover" />
+          <div className="h-full z-0 w-full absolute bg-gradient-to-b from-transparent to-black opacity-60"></div>
+          <div className="absolute bottom-1 px-4 py-3 transition-all duration-500 ease-out group-hover:translate-y-1 translate-y-14">
+            <p className="text-white font-semibold text-3xl z-10 line-clamp-2">
+              {title}
+            </p>
+            <p className="text-gray-200 text-justify mt-2 text-xs group-hover:opacity-100 opacity-0 transition-all z-10 line-clamp-3">
+              {body}
+            </p>
+          </div>
         </div>
       </div>
-    </div>
+    </Link>
   );
 };
 
