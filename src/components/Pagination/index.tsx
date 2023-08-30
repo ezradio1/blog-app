1024f9f1949cc58420f1a7883e78bdbecf9896e2
Ezra Audivano Dirfa <ezra@skyshi.com>
2023-08-30 17:28:37 +0700

    fix: modify pagination more functional

--- src/components/Pagination/index.tsx
+++ src/components/Pagination/index.tsx
@@ -1,30 +1,46 @@
-import React from "react";
-import { FiChevronLeft, FiChevronRight } from "react-icons/fi";
-import { PaginationProps } from "./index.types";
-import clsx from "clsx";
 import { GRAY, PRIMARY } from "@/constants/colors";
 import { noop } from "@/helpers/noop";
+import clsx from "clsx";
+import { FiChevronLeft, FiChevronRight } from "react-icons/fi";
+import { ELLIPSIS } from "./index.constants";
+import type { PaginationProps } from "./index.types";
 
 const Pagination = (props: PaginationProps) => {
   const { isPrevDisabled, isNextDisabled, currentPage, onChangePagination } =
     props;
 
-  const pageNumbers = Array.from(
-    { length: currentPage },
-    (_, index) => index + 1
+  let pageNumbers = Array.from({ length: currentPage }, (_, index) =>
+    String(index + 1)
   );
 
-  const actionClassName = (isDisabled: boolean) =>
+  if (pageNumbers.length > 9) {
+    const firstPart = pageNumbers.slice(0, 2);
+    const middlePart = [
+      ELLIPSIS,
+      ...pageNumbers.slice(
+        Math.max(0, currentPage / 2 - 1),
+        Math.min(currentPage, currentPage / 2 + 1)
+      ),
+      ELLIPSIS,
+    ];
+    const lastPart = pageNumbers.slice(-2);
+    pageNumbers = [];
+    pageNumbers.push(...firstPart, ...middlePart, ...lastPart);
+  }
+
+  const actionClassName = (isDisabled: boolean, page = "") =>
     clsx(
-      "border p-2 rounded transition-all w-10 h-10 flex justify-center items-center",
+      "rounded transition-all md:w-10 md:h-10 h-7 w-7 text-xs md:text-sm flex justify-center items-center",
       {
         "cursor-not-allowed": isDisabled,
         "cursor-pointer hover:border-primary border-gray-400": !isDisabled,
+        border: page !== ELLIPSIS,
+        "cursor-default": page === ELLIPSIS,
       }
     );
 
   return (
-    <div className="flex mt-4 gap-2">
+    <div className="flex mt-4 gap-1 md:gap-2">
       <div
         className={actionClassName(isPrevDisabled)}
         onClick={isPrevDisabled ? noop : () => onChangePagination("-")}
@@ -33,10 +49,14 @@ const Pagination = (props: PaginationProps) => {
       </div>
       {pageNumbers.map((page, key) => (
         <div
-          className={clsx(actionClassName(false), {
-            "bg-primary text-white": currentPage === page,
+          className={clsx(actionClassName(false, page), {
+            "bg-primary text-white": currentPage === Number(page),
           })}
-          onClick={isPrevDisabled ? noop : () => onChangePagination(page)}
+          onClick={
+            isPrevDisabled || page === ELLIPSIS
+              ? noop
+              : () => onChangePagination(Number(page))
+          }
           key={key}
         >
           <p>{page}</p>
--- src/components/Table/components/Pagination/index.types.ts
+++
@@ -1,8 +0,0 @@
-export interface PaginationProps {
-  isPrevDisabled: boolean;
-  isNextDisabled: boolean;
-  onChangePagination: (key: PaginationParams) => void;
-  currentPage: number;
-}
-
-export type PaginationParams = "+" | "-" | number;
