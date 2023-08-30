ad4bf5e0d46ebc09f2d2bd1496d7a5fc098683b1
Ezra Audivano Dirfa <ezra@skyshi.com>
2023-08-30 22:26:44 +0700

    style: logo alignment on sidebar mobile

--- src/components/Navbar/components/MobileMenu/index.tsx
+++ src/components/Navbar/components/MobileMenu/index.tsx
@@ -32,7 +32,7 @@ const MobileMenu = (props: MobileMenuProps) => {
           }
         )}
       >
-        <div className="flex px-4 py-2 justify-between items-center">
+        <div className="flex pl-4 py-2 justify-between items-center">
           <div className="flex items-center gap-2 cursor-pointer" onClick={handleClickLogo}>
             <Image src={AppLogo} alt="app-logo.png" width={30} height={30} />
             <h2 className="font-bold text-xs md:text-lg text-primary">
