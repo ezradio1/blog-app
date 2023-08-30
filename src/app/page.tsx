276a3fb17e29c5e6cac38dc492162b8bdc5b8c69
Ezra Audivano Dirfa <ezra@skyshi.com>
2023-08-30 18:54:00 +0700

    chore: dashboard metadata title

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ import DashboardComponent from "@/routes/Dashboard";
 import { Metadata } from "next";
 
 export const metadata: Metadata = {
-  title: "LuminaLife Blog | Dashboard",
+  title: "LuminaLife Blog",
   description:
     "Discover inspiration and insights for personal growth and holistic well-being at LuminaLife Dashboard. Explore mental health, self-development, balanced lifestyles, and more. Join us on the journey to a radiant life.",
   icons: "/src/app/favicon.ico",
